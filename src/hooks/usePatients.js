495e65637ee4103a3be07f23a966fe8534e32adb
manaschubby <manaschubby@gmail.com>
2023-06-28 10:48:26 +0530

    Singular Get Hook

--- src/hooks/usePatients.js
+++ src/hooks/usePatients.js
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react";
 const electron = window.require("electron");
 const { ipcRenderer } = electron;
-export default function usePatients() {
+export default function usePatients(id = null) {
 	const [patients, setPatients] = useState([]);
 	const [patientsLoaded, setPatientsLoaded] = useState(false);
 	const reloadPatients = () => {
 		ipcRenderer.send("get-patients");
 	};
 	useEffect(() => {
+		if (id) {
+			ipcRenderer.send("get-patient", id);
+			ipcRenderer.on("patient", (event, patient) => {
+				setPatients([JSON.parse(patient)]);
+				setPatientsLoaded(true);
+			});
+			return () => {
+				ipcRenderer.removeAllListeners("patient");
+			};
+		}
 		ipcRenderer.send("get-patients");
 		ipcRenderer.on("patients", (event, patients) => {
 			patients = JSON.parse(patients);
